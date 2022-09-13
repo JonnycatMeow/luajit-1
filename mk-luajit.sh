0a6681a69295422e299f86879822081fe38cc523
Randomxdp <Randomxdp>
2022-09-13 18:09:43 -0400

    debugging

--- mk-luajit.sh
+++ mk-luajit.sh
@@ -51,9 +51,8 @@ function check_NDK() {
 }
 # In a nutshell, we need these libs to load 32 bit minilua and compile for 32 bits
 function install_32bits() {
-    sudo dpkg --add-architecture i386
-    sudo apt-get install libc6:i386 libstdc++6:i386 
-    echo "Finished installing 32 bit libs!"
+    sudo dpkg --add-architecture i386 && echo "Added i386"
+    sudo apt-get install libc6:i386 libstdc++6:i386 && echo "Finished installing 32 bit libs!" 
 }
 ## NOTE: Since https://github.com/koreader/koreader-base/pull/1133, we append -DLUAJIT_SECURITY_STRHASH=0 -DLUAJIT_SECURITY_STRID=0 to TARGET_CFLAGS on !Android platforms.
 ##       Here, we leave it at the defaults, because we have much less control over the environment on Android, so, better be safe than sorry ;).
